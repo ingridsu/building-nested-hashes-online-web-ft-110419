111465fd7750b693a8a00d1fe7e70274cc207313
Ingrid sura <sura.ingrid@gmail.com>
2019-11-21 21:32:27 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,7 +7,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [{ 
-        name: "Banevolio", 
+        name: "Benvolio", 
         age: "17", 
         attitude: "worried"
         
