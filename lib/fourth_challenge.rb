ed16f2549088d8a3c46bedd1d7ccfdf29d4cfdce
Ingrid sura <sura.ingrid@gmail.com>
2019-11-21 21:20:44 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,10 +1,21 @@
 def fourth_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
+      :patriarch => {
+        name: "Lord Montague", 
+        age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{ name: "Banevolio", age: "17" attitude: "worried"}, { name: "Mercutio" age: "18" attitude: "hot-headed"}
+      :hero_friends => [{ 
+        name: "Banevolio", 
+        age: "17", 
+        attitude: "worried"
+        
+      }, 
+       { 
+        name: "Mercutio",
+        age: "18",
+        attitude: "hot-headed"}
         #your hashes here!
       ]
    }, 
